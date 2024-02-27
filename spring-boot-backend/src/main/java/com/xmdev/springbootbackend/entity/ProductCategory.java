1e5002fb243fcd7257a267926b001725e6597831
Xerckiem <mercadoxerckiem@gmail.com>
2024-02-27 01:46:03 -0500

    added entity files

--- spring-boot-backend/src/main/java/com/xmdev/springbootbackend/entity/ProductCategory.java
+++ spring-boot-backend/src/main/java/com/xmdev/springbootbackend/entity/ProductCategory.java
@@ -1,2 +1,27 @@
-package com.xmdev.springbootbackend.entity;public class ProductCategory {
+package com.xmdev.springbootbackend.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Set;
+
+@Entity
+@Table(name="product_category")
+// @Data -- known bug with Lombok and Hibernate
+@Getter
+@Setter
+public class ProductCategory {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    @Column(name="id")
+    private Long id;
+
+    @Column(name="category_name")
+    private String categoryName;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "category")
+    private Set<Product> products;
 }
